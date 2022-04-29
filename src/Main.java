69c9e92ab8f544b2751665d0e56dea8a8d50c92c
abdullinru <abdullin17021989@gmail.com>
2022-04-29 15:52:09 +0600

    Третье задание исправил

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         task3();
@@ -7,11 +9,10 @@ public class Main {
         int currentYear = 2022;
         int startYear = currentYear-200;
         int endYear = currentYear+100;
-        int i = startYear+79;
-        while(i<endYear){
-            System.out.println(i);
-            i+=79;
-
+        for (int year = startYear; year < endYear; year++){
+            if (year%79==0){
+                System.out.println(year);
+            }
         }
     }
 }
