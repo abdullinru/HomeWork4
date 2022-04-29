79b0d616f3597af716bc3280caafd0d2cabc9bc9
abdullinru <abdullin17021989@gmail.com>
2022-04-29 15:11:38 +0600

    первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
 
-        System.out.println("Hello world!");
+    }
+    public static void task1(){
+        int i =1;
+        while (i<11){
+            System.out.print(i++ + " ");
+        }
+        System.out.println();
+        i=10;
+        for (;i>0;i--){
+            System.out.print(i + " ");
+        }
     }
 }
