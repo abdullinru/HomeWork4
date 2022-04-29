bd6e1bddf6bede9128bf0b7dbd39151bd7502810
abdullinru <abdullin17021989@gmail.com>
2022-04-29 15:57:49 +0600

    Третье задание правка уменьшил число итераций

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,16 @@ public class Main {
 
     }
     public static void task3(){
-        int currentYear = 2022;
+        int currentYear = LocalDate.now().getYear();;
         int startYear = currentYear-200;
         int endYear = currentYear+100;
-        for (int year = startYear; year < endYear; year++){
+        for (int year = startYear; year <= endYear; year++){
             if (year%79==0){
-                System.out.println(year);
+                while (year<=endYear) {
+                    System.out.println(year);
+                    year = year + 79;
+                }
+                break;
             }
         }
     }
